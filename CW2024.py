d4c6791e64f4bd0e8b5ed29a5ab57404d8857d56
D.G.C Anupama <chamathanupama3@gmail.com>
2025-09-18 00:30:59 +0530

    Initial Commit

---
+++ CW2024.py
@@ -0,0 +1,15 @@
+usa=['Sri Lanka>UK>USA','Sri Lanka>Japan>USA','Sri Lanka>Singapore>Japan>USA']
+Japan=['Sri Lanka>Japan','Sri Lanka>Singapore>Japan']
+aus=['Sri Lanka>Australia','Sri Lanka>singapore>Australia','Sri Lanka>singapore>Japan>Australia','Sri Lanka>Japan>Australia'] 
+sing=['Singapore>Australia','Singapore>Japan','Singapore>Japan>USA','Singapore>Japan>Australia']
+jap=['Japan>USA','Japan>Australia']
+uk="Sri Lanka>UK"
+sing_1="Sri Lanka>Singapore"
+uk_1="UK>USA"
+ 
+
+
+
+
+
+
